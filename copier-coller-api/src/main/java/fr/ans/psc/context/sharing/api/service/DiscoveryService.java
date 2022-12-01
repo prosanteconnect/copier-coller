91d3d103b8ea79c1536281da04fc9d1049d0a863
bbo <bbonche@henix.fr>
2022-12-01 13:39:52 +0100

    initialize API repo

---
+++ copier-coller-api/src/main/java/fr/ans/psc/context/sharing/api/service/DiscoveryService.java
@@ -0,0 +1,21 @@
+package fr.ans.psc.context.sharing.api.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DiscoveryService {
+
+    public List<String> getAllSchemas() {
+        return null;
+    }
+
+    public String getPscJsonContextSchema() {
+        return null;
+    }
+
+    public void createPscContextJsonSchema(String schemaString) { }
+
+    public void updatePscContextJsonSchema(String schemaString) { }
+}
