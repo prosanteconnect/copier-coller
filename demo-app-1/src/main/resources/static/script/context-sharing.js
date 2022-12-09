66f9f19b19d08163c7b5dd7ccc8e61ba739d7d01
bbo <bbonche@henix.fr>
2022-12-09 14:32:27 +0100

    variabilize mapping file in script to use it with different mappings in the application

--- demo-app-1/src/main/resources/static/script/context-sharing.js
+++ demo-app-1/src/main/resources/static/script/context-sharing.js
@@ -1,5 +1,5 @@
 let pscContext;
-const mappingUrl = window.location.origin + '/patient-info-mapping.json'
+// const mappingUrl = window.location.origin + '/patient-info-mapping.json'
 
 function getFromCache(serverURL) {
     $.get(serverURL, function (data) {
@@ -17,8 +17,8 @@ function getFromCache(serverURL) {
     });
 }
 
-function fillForm() {
-    $.getJSON(mappingUrl, function(data) {
+function fillForm(mappingFilePath) {
+    $.getJSON(window.location.origin + mappingFilePath, function(data) {
         for (const [key, value] of Object.entries(data)) {
             if (document.getElementById(key)) {
                 $('#' + key).val(_.get(pscContext, value, ''))
@@ -27,10 +27,10 @@ function fillForm() {
     })
 }
 
-function putInCache(schemaName, serverURL, viewURL) {
+function putInCache(schemaName, serverURL, viewURL, mappingFilePath) {
     let putPscContext = {};
 
-    $.getJSON(mappingUrl, function (data) {
+    $.getJSON(window.location.origin + mappingFilePath, function (data) {
         for (const [key, value] of Object.entries(data)) {
             if (document.getElementById(key)) {
                 _.set(putPscContext, value, document.getElementById(key).value)
